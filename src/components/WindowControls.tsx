9407627df0a6af0a08dbf0d9b2bbde8f947750fc
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-04-29 12:29:24 +0530

    feat: implement desktop environment components, add constants, and update project configuration

---
+++ src/components/WindowControls.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import useWindowStore from "#store/window";
+
+interface WindowControlsProps {
+  target: string;
+}
+
+const WindowControls = ({ target }: WindowControlsProps) => {
+  const { closeWindow } = useWindowStore();
+
+  return (
+    <div id="window-controls">
+      <div
+        className="close"
+        onClick={() => closeWindow(target)}
+      />
+      {/* <div className="minimize" />
+            <div className="maximize" /> */}
+    </div>
+  );
+};
+
+export default WindowControls;
